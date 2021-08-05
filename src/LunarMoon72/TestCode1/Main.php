ca38276554735fa2c42797885763baca4135cc05
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-05 17:40:49 -0400

    Created Main.php
    Will it work.... Wait dev tools is broken ;-;

--- src/LunarMoon72/TestCode1/Main.php
+++ src/LunarMoon72/TestCode1/Main.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace LunarMoon72;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+  public function onLoad(){
+    $this->getLogger()->info("Plugin is LOADING");
+  }
+  
+  public function onEnabled(){
+    $this->getLogger()->info("Plugin is ENABLED");
+  }
+  
+  public function onDisabled(){
+    $this-getLogger()->info("Plugin is DISABLED");
+  }
+  
+}
